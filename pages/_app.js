acfd9cb402e3a87e413e2fb22c806db2b7944f82
Undeinf <www.undefined@gmail.com>
2023-04-28 17:03:29 +0530

    Added assets and icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import Navbar from '../components/navbar';
 import "../styles/globals.css";
+import Head from 'next/head';
 
 function App({Component, pageProps}){
     return (
         <>
+        <Head>
+            <link rel="icon" href="/icons/favicon.ico"/>
+        </Head>
          <header>
             <Navbar />
         </header>
